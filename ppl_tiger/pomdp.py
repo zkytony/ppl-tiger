942c8a232a77fc188a520107ff89d83a21af5fdc
Kaiyu Zheng <kaiyutony@gmail.com>
2020-09-17 11:37:24 -0400

    belief update is ok

--- ppl_tiger/pomdp.py
+++ ppl_tiger/pomdp.py
@@ -69,99 +69,44 @@ def reward_model(state, action):
 def uniform_policy_model():
     return dist.Categorical(tensor([1.,1.,1.]))
 
-def expected_reward_model(state, policy_model, t=0,
-                          discount=1.0, discount_factor=0.95, thresh=1e-4,
-                          suffix=""):
-    if discount < thresh:
-        return dist.Delta(tensor(0.0))
-    action = actions[pyro.sample("a-%s_%d" % (suffix, t), policy_model())]
-    reward = pyro.sample("r-%s_%d" % (suffix, t), reward_model(state, action))
-    next_state = states[pyro.sample("s-%s_%d" % (suffix, t+1),
-                                    transition_model(state, action))]
-    observation = observations[pyro.sample("o-%s_%d" % (suffix, t),
-                                           observation_model(next_state, action))]
-    future_reward_dist = expected_reward_model(next_state,
-                                               policy_model,
-                                               t=t+1,
-                                               discount=discount*discount_factor,
-                                               discount_factor=discount_factor,
-                                               thresh=thresh)
-    cum_reward = reward + discount*pyro.sample("r-future-%s_%d" % (suffix, t),
-                                               future_reward_dist)
-    return dist.Delta(cum_reward)
 
-def belief_update(belief, observation, action):
-    belief_probs = torch.zeros(len(states))
-    for s_next, next_state in enumerate(states):
-        trans_prob = 0.0
-        for j, state in enumerate(states):
-            trans_prob += transition_model(state, action).log_prob(tensor(s_next))
-        obs_prob = observation_model(next_state, action).log_prob(tensor(observations.index(observation)))
-        belief_probs[s_next] = obs_prob * trans_prob
-    return dist.Categorical(belief_probs)
+def Infer(svi, *args, num_steps=100, print_losses=True, **kwargs):
+    losses = []
+    for t in range(num_steps):
+        losses.append(svi.step(*args, **kwargs))
+        if print_losses:
+            print("Loss [%d] = %.3f" % (t, losses[-1]))
 
-def expected_belief_reward_model(belief, policy_model, t=0,
-                                 discount=1.0, discount_factor=0.95, thresh=1e-4,
-                                 num_samples=5):
-    if discount < thresh:
-        return dist.Delta(tensor(0.0))
 
-    cum_reward = 0.0
-    for i in range(num_samples):
-        state = states[pyro.sample("s-%d_%d" % (i, t), belief)]
-        action = actions[pyro.sample("a-%d_%d" % (i, t), policy_model())]
-        reward = pyro.sample("r-%d_%d" % (i, t), reward_model(state, action))
-        next_state = states[pyro.sample("s-next-%d_%d" % (i, t+1), transition_model(state, action))]
-        observation = observations[pyro.sample("o-%d_%d" % (i, t), observation_model(next_state, action))]
-        next_belief = belief_update(belief, observation, action)
-        future_reward_dist = expected_belief_reward_model(next_belief,
-                                                          policy_model,
-                                                          t=t+1,
-                                                          discount=discount*discount_factor,
-                                                          discount_factor=discount_factor,
-                                                          thresh=thresh,
-                                                          num_samples=num_samples)
-        cum_reward += belief.log_prob(tensor(states.index(state)))\
-            * (reward + discount*pyro.sample("r-future-%d_%d" % (i, t),
-                                             future_reward_dist))
-        print(i)
-    return dist.Delta(cum_reward)
+def belief_update(belief, action, observation, num_steps=100):
+    def belief_update_model(belief, action, observation):
+        state = states[pyro.sample("state", belief)]
+        next_state = states[pyro.sample("next_state", transition_model(state, action))]
+        with pyro.condition(data={"obs": observation}):
+            predicted_observation = pyro.sample("obs", observation_model(next_state, action))
 
-print(expected_reward_model("tiger-left", uniform_policy_model))
+    def belief_guide(belief, action, observation):
+        belief_weights = pyro.param("belief_weights", torch.ones(len(states)),
+                                    constraint=dist.constraints.simplex)
+        state = states[pyro.sample("state", dist.Categorical(belief_weights))]
+        next_state = states[pyro.sample("next_state", transition_model(state, action))]
+
+    svi = pyro.infer.SVI(belief_update_model,
+                         belief_guide,
+                         pyro.optim.Adam({"lr": 0.001}),
+                         loss=pyro.infer.Trace_ELBO())
+    if type(observation) == str:
+        # !! Having to call observations.index is really annoying. I wish
+        # Pyro can directly work with strings / or tensors support strings. !!
+        # ... To work around this, either your models only work with integers
+        # or you call index every time everywhere.
+        observation = tensor(observations.index(observation))
+    Infer(svi, belief, action, observation, num_steps=num_steps)
+    return dist.Categorical(pyro.param("belief_weights"))
+
 prior_belief = dist.Categorical(tensor([1., 1.]))
-print(expected_belief_reward_model(prior_belief, uniform_policy_model))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # World Model
-# # Pr (b, o, a, r)
-# # Observable: b0, rewards
-# # I think the example on Agent Book (https://agentmodels.org/chapters/3c-pomdp.html)
-# # is wrong because from the perspective of the POMDP agent it
-# # cannot simulate forward observations based on the real state!
-# # ... I realize they are trying to simulate both the world and
-# # the agent together instead of just doing POMDP planning
-# def step_model(belief, t=0):
-#     """
-#     belief is a distribution.
-
-#     The world model essentially samples an action to take
-#     from a categorical distribution, then computes the
-#     expected reward R(b,a) where b' is the updated belief.
-#     """
-#     action = actions[
-#         pyro.sample("a_%d" % t,
-#                     dist.Categorical(tensor([1.,1.,1.])))]
+action = "listen"
+observation = "growl-left"
+new_belief = belief_update(prior_belief, action, observation, num_steps=100)
+for name in pyro.get_param_store():
+    print("{}: {}".format(name, pyro.param(name)))
